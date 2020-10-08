b41ac34d53645f722dc96e8c55d5ca52e4d3e46a
simonacossai <cossaisimona@gmail.com>
2020-10-08 20:43:29 +0200

    DONE- creating table cell and selecting on button click random numbers

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
-let numbers = [];
-const tableNumbers = function () {
-  const availableNumbers = []
-  for (let i=1; i<=76; i++){
-      availableNumbers.push(i);
-  }
-  return availableNumbers;
-};
-
-window.onload = function () {
-  const num = tableNumbers(); 
-  console.log(num);
-  let table = document.querySelector("#table"); 
-  for (let i = 0; i < num.length; i++) {
-    numbers[i] = []; 
-
-    const child = document.createElement("div"); 
-    child.className = "day"; 
-    const numH3 = document.createElement("h3");
-    const totalNumberCells = i + 1;
-    numH3.innerText = totalNumberCells;
-    child.appendChild(numH3);
-    table.appendChild(child);
-  }
-
-const random = document.querySelector('#random');
-  random.onclick = function (event) {
-  
-    let selected = document.querySelector(".selected");
-    if (selected) {
-      selected.classList.remove("selected");
+const createCells = function () {
+    const board = document.querySelector("#appointments");
+     // container for my cells
+    for (let i = 1; i <= 76; i++) {
+        const cell = document.createElement("div") // create empty cell
+        cell.classList.add('bingocell')
+        cell.innerText = i
+        board.appendChild(cell)
     }
-    event.currentTarget.classList.add("selected");
-  };
-};
+}
+
+const randomizeNumber = function () {
+    let number = Math.floor(Math.random() * 76) + 1
+    console.log(number)
+    let allTheCells = document.querySelectorAll('.bingocell')
+    let foundCell;
+    //for (let y = 0; y < 76; y++) {
+    //    if (allTheCells[y].innerText === number.toString()) {
+    //        foundCell = allTheCells[y]
+    //    }
+    //}
+    console.log(foundCell)
+    allTheCells[number - 1].classList.add("selected")
+    //foundCell.style.backgroundColor = 'yellow'
+}
+
+window.onload = createCells
